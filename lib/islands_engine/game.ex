4f076b6d2fdc6f6240626b1ab2b903c4d6e92b42
Yin Weijun <gw9812@gmail.com>
2017-04-07 10:53:29 +0800

    add a new player

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -9,6 +9,10 @@ defmodule IslandsEngine.Game do
     GenServer.call(game, :demo)
   end
 
+  def add_player(pid, name) when name != nil do
+    GenServer.call(pid, {:add_player, name})
+  end
+
   def start_link(name) when not is_nil(name) do
     GenServer.start_link(__MODULE__, name)
   end
@@ -19,9 +23,14 @@ defmodule IslandsEngine.Game do
       {:ok, %Game{player1: player1, player2: player2}}     
     end
   end
-  
+
   def handle_call(:demo, _from, state) do
     {:reply, state, state}
+  end
+
+  def handle_call({:add_player, name}, _from, state) do
+    Player.set_name(state.player2, name)
+    {:reply, :ok, state}
   end
 
 end
